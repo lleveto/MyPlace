c8f7ab27451a5c5a618d37299ed1669138f81ed8
Liam Leveto <lleveto@AD.Cuyahoga.CC>
2023-08-24 14:41:27 -0400

    Updated index.html

--- GenerateEncodedURL.js
+++
@@ -1,7 +0,0 @@
-<script>
-function openEncodedURL(parcelpin) {
-  var encodedParcelID = encodeURIComponent(parcelpin);
-  var url = "https://myplace.cuyahogacounty.us/" + encodedParcelID;
-  window.open(url, "_blank");
-}
-</script>
